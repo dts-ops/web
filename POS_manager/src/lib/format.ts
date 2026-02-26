af4e613b12deea60092e121405ca242331c8516f
dts <trungson1812cpr@gmail.com>
2026-02-26 23:17:03 +0700

    combo fix

--- POS_manager/src/lib/format.ts
+++
@@ -1,5 +0,0 @@
-export function formatVND(amount: number): string {
-  const rounded = Math.round(amount);
-  const formatted = rounded.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  return `${formatted} ₫`;
-}
